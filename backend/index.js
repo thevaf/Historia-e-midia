6cb76250b2a18e6fe9f41d23cf73f269a674db16
Vaf1896 <94250218+thevaf@users.noreply.github.com>
2024-10-06 14:29:33 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ fastify.post('/postblog', function handler (request, reply) {
    }
 })
 
-fastify.listen({ port: process.env.PORT || 4000 }, (err) => {
+fastify.listen({ port: process.env.PORT || 4000, host: "0.0.0.0" }, (err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
